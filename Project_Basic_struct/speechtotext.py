e531a4056699fe4917fae16d5ed13175c1f67dec
Soham Ratnaparkhi <soham.ratnaparkhi@gmail.com>
2022-01-23 15:38:22 +0530

    commit 1

---
+++ Project_Basic_struct/speechtotext.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+# initialize the recognizer
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    # read the audio data from the default microphone
+    audio_data = r.record(source, duration=5)
+    print("Recognizing...")
+    # convert speech to text
+    text = r.recognize_google(audio_data)
+    print(text)
