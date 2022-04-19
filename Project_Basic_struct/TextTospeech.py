74b522d5f31c196e9d26e02985627a6a7cb6afdd
Soham Ratnaparkhi <soham.ratnaparkhi@gmail.com>
2022-04-19 14:25:51 +0530

    Update TextTospeech.py

--- Project_Basic_struct/TextTospeech.py
+++ Project_Basic_struct/TextTospeech.py
@@ -8,14 +8,8 @@ def tts():
     audio = 'speech.mp3'
     language = 'en'
     sentence = input("Enter the text to be spoken :- ")
-    # choice = input("Do you want a male voice or a female voice ? (M/F) ")
-    # choice = choice.lower()
+    
     speaker = win32com.client.Dispatch("SAPI.SpVoice")
-    # #if choice == 'f':
-    #     sp = gTTS( text = sentence, lang = language, slow = False )
-    #     sp.save(audio)
-    #     playsound(audio)  
-    # elif choice == 'm':
     sp = speaker.Speak(sentence)
     
 
